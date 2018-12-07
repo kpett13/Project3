e5c4298324e1a657817c35a2e35547983480f3d6
kpett13 <44513119+kpett13@users.noreply.github.com>
2018-12-07 15:48:52 -0500

    Update Heating Loop.py

--- Heating Loop.py
+++ Heating Loop.py
@@ -57,7 +57,7 @@ WF_3 = ct.Water()
 WF_4 = ct.Water()
 
 "Knowns"
-T1_air = 0+273           # T1 ambient = 0C
+T1_air = 0+273            # T1 ambient = 0C
 P1_air = 1*10**5          # P1 ambient = 1 bar
 T2_air = 40+273           # T2 into cabin = 40C
 P2_air = P1_air           # P2 = 1 bar
@@ -69,11 +69,11 @@ h2_air = air_2.h          ###
 "Set variables"
 voldot_air = 500          # Volumetric fow rate of air into cabin (m^3/hr)
 D_Gasline = 2             # Inner diameter of gas line (in)
-D_Liqline = 2             # Inner diameter of liquid line (in)
+D_Liqline = .5            # Inner diameter of liquid line (in)
 q_evaporator = 2500       # Heat rejected from 
 n_pump = 0.95             # Pump efficiency
-P1 = 0.5*10**4            # Low side pressure
-pr = 18                   # Pressure ratio
+P1 = 0.6*10**4            # Low side pressure
+pr = 20                   # Pressure ratio
 
 "Calculations from set variables"
 mdot_air = voldot_air*(air_1.density)*1/3600       # Mass flow rate of air into cabin
@@ -83,22 +83,14 @@ Ac_Gasline = (math.pi/4)*((0.0254)*D_Gasline)**2   # Cross sectional area of 2" 
 
 
 for mdot in range(1,10):
-    
-    mdot_WF = (mdot/500)   # Define actual mdot of working fluid (kg/s)
-    
-    "State 1 - Outlet Throttle / Inlet Evaporator"
-    X1 = 0                 # Fluid is assumed to be a saturated liquid
-    WF_1.PX = P1, 1        # Define state [P1 is chosen out of loop.] 
-    T1 = WF_1.T            ###
-    s1 = WF_1.s            ### Get rest of paramaters
-    h1 = WF_1.h            ###
+ 
+    mdot_WF = (mdot/272)   # Define actual mdot of working fluid (kg/s)
     
     "State 2 - Outlet Evaporator / Inlet Pump"
     P2 = P1                # Heat addition is assumed to be isobaric
-    h2 = (q_evaporator/mdot_WF)+h1   # Heat from the source is q = mdot*(delta:h)          
-    WF_2.HP = h2,P2        # Define state
-    h2 = WF_2.h            ###
-    X2 = WF_2.X            ### Get rest of paramaters    
+    X2 = 1                 # Assume saturated vapor
+    WF_2.PX = P2, X2       # Define state
+    h2 = WF_2.h            ### Get rest of paramaters    
     T2 = WF_2.T            ### 
     s2 = WF_2.s            ###
 
@@ -106,7 +98,7 @@ for mdot in range(1,10):
     P3 = pr*P2             # Pressure is increased by chosen pressure ratio to P1
     s3_is = s2             # First, assume pump to be isentropic 
     WF_3.SP = s3_is,P3     # Define isentropic outlet state
-    h3_is = WF_2.h         # Define isentropic outlet enthalpy
+    h3_is = h2             # Define isentropic outlet enthalpy
     h3 = h_OutCompressor(n_pump, h3_is, h2)    # Define actual outlet enthalpy using pump efficiency
     WF_3.HP = h3,P3                            # Define state
     X3 = WF_3.X            ###
@@ -121,25 +113,25 @@ for mdot in range(1,10):
     T4 = WF_4.T            ### Get rest of paramaters
     s4 = WF_4.s            ###
     
-#    #Effectiveness of Radiatior
-#    air_2_perf = ct.Solution('air.cti')     # I THINK THIS IS WRONG
-#    T2_air_perf = T3
-#    air_2_perf.TP = T2_air_perf,P2_air
-#    h2_air_perf = air_2_perf.h
-#    e_Rad = (h2_air-h1_air)/(h2_air_perf-h1_air)
-    
+    "State 1 - Outlet Throttle / Inlet Evaporator"
+    h1 = -(q_evaporator/mdot_WF)+h2    # Heat from the source is q = mdot*(delta:h)
+    WF_1.HP = h1,P1                    # Define state [P1 is chosen out of loop.] 
+    T1 = WF_1.T            ###
+    s1 = WF_1.s            ### Get rest of paramaters
+    X1 = WF_1.X            ###    
+
     #Effectiveness of Radiatior
-#    WF_4_perf =  ct.Hfc134a()
     WF_4_perf =  ct.Water()
     T4_perf = T1_air+1        #effectiveness is rough bc water cannot be solid
     WF_4_perf.TP = T4_perf,P4
     h4_perf = WF_4_perf.h
     e_Rad = (h4-h3)/(h4_perf-h3)
-        
+    rho4 = WF_4.density
+    rho2 = WF_2.density
     
     _mdot.append(mdot_WF)    
-    _Vel_Liq.append(mdot_WF/(Ac_Liqline*WF_1.density))  
-    _Vel_Gas.append(mdot_WF/(Ac_Gasline*WF_3.density))
+    _Vel_Liq.append(mdot_WF/(Ac_Liqline*rho4))  
+    _Vel_Gas.append(mdot_WF/(Ac_Gasline*rho2))
     _e_Rad.append(e_Rad)
     _T1.append(T1)
     _T2.append(T2)
